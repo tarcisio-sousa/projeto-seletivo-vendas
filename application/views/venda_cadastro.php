1adcf1ec41012817365fbb7ccf0df5fb9c14478f
tarcisio <sousa.tarcisio.s@gmail.com>
2019-04-28 19:40:00 -0300

    Configurando Script select Cliente em Vendas

--- application/views/venda_cadastro.php
+++ application/views/venda_cadastro.php
@@ -15,21 +15,28 @@
 			</nav>
 		</div>
 	</div>
-	<div class="alert alert-warning validate mt-0 pt-1 pb-1 text-left" style="display: none;" role="alert"></div>
 	<div class="row">
 		<div class="col-md-3">
 		</div>
 		<div class="col-md-6">
+		<div class="alert validate mt-0 pt-1 pb-1" style="display: none;" role="alert"></div>
 			<form onsubmit="return(false);" role="form" id="formVenda">
 			<? // form_open('venda/salvar') ?>
 				<div class="form-group mb-2 text-left">
 					 
 					<div class="row">
-						<div class="col-sm-2">
+						<div class="col-sm-3">
 							<label for="cliente"> Cliente </label>
-							<input type="text" name="cliente" id="cliente" class="form-control" autofocus="true" />
+							<div class="input-group">
+								<input type="text" name="cliente" id="cliente" class="form-control" autofocus="true" />
+								<div class="input-group-append">
+									<a class="btn btn-outline-secondary modalCliente" href="#" data-toggle="modal" data-target="#clienteModal">
+									  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-0" role="img" viewBox="0 0 24 24" focusable="false"><circle cx="10.5" cy="10.5" r="7.5"></circle><path d="M21 21l-5.2-5.2"></path></svg>
+									</a>
+								</div>
+							</div>
 						</div>
-						<div class="col-sm-10">
+						<div class="col-sm-9">
 							<label for="cliente"> Nome Cliente </label>
 							<input type="text" name="nome_cliente" id="nome_cliente" class="form-control" />
 						</div>
@@ -40,11 +47,18 @@
 				<div class="form-group mb-2 text-left">
 					 
 					<div class="row">
-						<div class="col-sm-2">
-							<label for="produto"> Produto </label>
-							<input type="text" name="produto" id="produto" class="form-control" />
+						<div class="col-sm-3">
+								<label for="produto"> Produto </label>
+								<div class="input-group">
+									<input type="text" name="produto" id="produto" class="form-control" />
+									<div class="input-group-append">
+										<a class="btn btn-outline-secondary" href="#">
+										  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-0" role="img" viewBox="0 0 24 24" focusable="false"><circle cx="10.5" cy="10.5" r="7.5"></circle><path d="M21 21l-5.2-5.2"></path></svg>
+										</a>
+									</div>
+								</div>
 						</div>
-						<div class="col-sm-10">
+						<div class="col-sm-9">
 							<label for="produto"> Descrição Produto </label>
 							<input type="text" name="descricao_produto" id="descricao_produto" class="form-control" />
 						</div>
@@ -56,7 +70,7 @@
 						<div class="form-group mb-2 text-left">
 							 
 							<label for="forma_pagamento"> Pagamento </label>
-							<select class="form-control" id="forma_pagamento">
+							<select class="form-control custom-select d-block w-100" id="forma_pagamento">
 							<?php foreach($formas_pagamento as $fpg): ?>
 								<option value="<?= $fpg?>"><?= $fpg?></option>
 							<?php endforeach; ?>
@@ -107,11 +121,78 @@
 	</div>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="clienteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Selecionar Cliente</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-search mt-3 ml-3 mb-0 mr-3">
+    	<div class="input-group">
+			<input type="text" name="select_cliente" id="select_cliente" class="form-control">
+			<div class="input-group-append">
+				<button type="button" id="button_select_cliente" class="btn btn-primary"> <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-0" role="img" viewBox="0 0 24 24" focusable="false"><circle cx="10.5" cy="10.5" r="7.5"></circle><path d="M21 21l-5.2-5.2"></path></svg> </button>
+			</div>
+    	</div>
+        
+      </div>
+      <div class="modal-body">
+        
+        <div id="result_select_cliente">
+        	<table class="table table-sm" style="display: none;">
+        		<thead>
+		    		<tr>
+		    			<th>Nome</th>
+		    			<th>CPF</th>
+		    			<th></th>
+		    		</tr>
+        		</thead>
+        		<tbody>
+        		</tbody>
+        	</table>
+        </div>
+      </div>
+        <!-- 
+      <div class="modal-footer">
+        	<button type="button" class="btn btn-primary btn-sm">Save changes</button>
+        	 -->
+      </div>
+    </div>
+  </div>
+</div>
+
 <script>
 $(document).ready(function(){
 
-	$("#cliente").blur(function(){
+	$("#button_select_cliente").click(function(){
 
+		$.getJSON("<?= base_url(); ?>cliente/select/" + $("#select_cliente").val(), function(result){
+			
+			$("#result_select_cliente table tbody").text("");
+			$.each(result, function(i, field){
+				if(!result["id"]){
+			    	$("#result_select_cliente table tbody").append("<tr><td>" + field["nome"] + "</td><td>" + field["cpf"] + "</td><td class='text-right'><a href='#' class='btn btn-secondary btn-sm select_cliente_button'>"+ field["id"] +"</a></td></tr>");
+			    	$("#result_select_cliente table").show();
+				} else {
+			    	$("#result_select_cliente table").hide();
+				}
+		    });
+
+			$(".select_cliente_button").click(function(){
+
+				$("#cliente").val($(this).text()).focus();
+
+				$("#clienteModal").modal("hide");
+			});
+		});
+	});
+
+
+	$("#cliente").blur(function(){
 
 		$.getJSON("<?= base_url(); ?>cliente/get/" + $("#cliente").val(), function(result){
 			if(result["id"] > 0) {
@@ -190,11 +271,28 @@ $(document).ready(function(){
 			function(data, status){
 				$(".alert").show(0, "swing", function(){
 					$(this).text(data["validate"]);
+					if(!data['error']){
+						$("#produto").val("");
+						$("#descricao_produto").val("");
+						$("#cliente").val("");
+						$("#nome_cliente").val("");
+						$("#forma_pagamento").val("DINHEIRO");
+						$("#quantidade").val("1");
+						$("#preco").val("0.00");
+						$("#valor_total").val("0.00");
+						$(this).removeClass("alert-warning");
+						$(this).addClass("alert-success");
+					} else {
+						$(this).removeClass("alert-success");
+						$(this).addClass("alert-warning");
+					}
 				});
 
 			},
 			"json"
 		);
 	});
+
+	// $(".modalCliente").
 });
 </script>
